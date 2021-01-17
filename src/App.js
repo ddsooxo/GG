1b49171f9e507896557552fa8f3a06274bdb3fb7
ddsooxo <dchuteng@gmail.com>
2021-01-16 23:46:00 -0800

    added text for landing page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Button from "./Components/Button/Button";
 import BetList from "./Components/BetList/BetList";
 import logo1 from "./assets/img/GG-Logo.png";
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -62,11 +61,18 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <title>GG</title>
-        <img class='logo' src={logo1} />
+        <title>greater good</title>
+        <img class="logo" src={logo1} alt="GG-Logo" />
         <header id="header">
-          <h1>GG</h1>
+          <h1>greater good</h1>
         </header>
+        <div class="v9_1">
+          <span class="v9_17">
+            be a helping hand for yourself and the world
+          </span>
+          <span class="v9_15">Sign In</span>
+        </div>
+
         <p>Competing with your friends for the Greater Good</p>
         <div className="btn-group">
           <Button
@@ -84,6 +90,18 @@ class App extends React.Component {
             onRemove={this.removeBet}
           />
         </section>
+
+        <div class="footer">
+          <span>
+            <a class="footer-about" href="">
+              {" "}
+              About{" "}
+            </a>
+          </span>
+
+          <a class="footer-contact" href="">  Contact  </a>
+          <p> made with love + hard work | © HackingHornets </p>
+        </div>
       </div>
     );
   }
